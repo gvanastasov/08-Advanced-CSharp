a3771749c9c699b6f2c2f53dda09b6d6dbfd88c7
gva <gvanastasov@abv.bg>
2017-05-29 23:08:48 +0200

    finished task 5

---
+++ 01_Stacks_Queues/stacks_queues/HotPotato/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HotPotato
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var names = Console.ReadLine().Split(' ');
+            var n = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<string>(names);
+            while (queue.Count != 1)
+            {
+                var toss = 1;
+                while (true)
+                {
+                    var tosser = queue.Dequeue();
+
+                    if(toss == n)
+                    {
+                        Console.WriteLine($"Removed {tosser}");
+                        break;
+                    }
+
+                    queue.Enqueue(tosser);
+                    toss++;
+                }
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
