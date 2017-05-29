08cded6a7df552dc5defe40792e4aa7bf83b8d65
gva <gvanastasov@abv.bg>
2017-05-29 23:42:13 +0200

    finished ex 1

---
+++ 01_Stacks_Queues/stacks_queues/ReverseNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReverseNumbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ');
+            var stk = new Stack<string>(numbers);
+
+            while (stk.Count != 1)
+            {
+                Console.Write(stk.Pop() + ' ');
+            }
+            Console.Write(stk.Pop());
+
+            Console.WriteLine();
+        }
+    }
+}
