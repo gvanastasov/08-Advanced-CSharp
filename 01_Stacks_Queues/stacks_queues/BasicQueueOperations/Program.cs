2003d1bd67563aaccaa9e9c58c32c9d983ad1bf2
gva <gvanastasov@abv.bg>
2017-05-30 00:15:22 +0200

    task 4 done

---
+++ 01_Stacks_Queues/stacks_queues/BasicQueueOperations/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace BasicQueueOperations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputs = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse);
+
+            var n = inputs[0];
+            var s = inputs[1];
+            var x = inputs[2];
+
+            var queue = new Queue<int>(numbers);
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if(queue.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if(queue.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    var min = int.MaxValue;
+                    while (queue.Count != 0)
+                    {
+                        var del = queue.Dequeue();
+                        if(del < min)
+                        {
+                            min = del;
+                        }
+                    }
+                    Console.WriteLine(min.ToString());
+                }
+            }
+        }
+    }
+}
