424a0c26d308ed5f0a9e420ae9247e3942c75f94
gva <gvanastasov@abv.bg>
2017-05-29 22:55:23 +0200

    task 4 done

---
+++ 01_Stacks_Queues/stacks_queues/MatchingBrackets/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MatchingBrackets
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var str = Console.ReadLine();
+
+            var stack = new Stack<int>();
+            for(var i = 0; i < str.Length; i++)
+            {
+                var symbol = str[i];
+                if(symbol == '(')
+                {
+                    stack.Push(i);
+                } 
+                else if(symbol == ')')
+                {
+                    var startIdx = stack.Pop();
+                    var len = i - startIdx + 1;
+
+                    var sub = str.Substring(startIdx, len);
+                    Console.WriteLine(sub);
+                }
+            }
+        }
+    }
+}
