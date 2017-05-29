0a6347b6d0cccbcf4030097d7a40f418d981e5a4
gva <gvanastasov@abv.bg>
2017-05-30 00:07:55 +0200

    task 3 done

---
+++ 01_Stacks_Queues/stacks_queues/MaximumElement/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace MaximumElement
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var cmdCount = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+
+            var maxes = new Stack<int>();
+            maxes.Push(int.MinValue);
+
+            for (int i = 0; i < cmdCount; i++)
+            {
+                var cmdTokens = Console.ReadLine().Split(' ');
+                var cmd = int.Parse(cmdTokens[0]);
+
+                switch (cmd)
+                {
+                    case 1:
+                        {
+                            var val = int.Parse(cmdTokens[1]);
+                            stack.Push(val);
+
+                            if(val > maxes.Peek())
+                            {
+                                maxes.Push(val);
+                            }
+                        }
+                        break;
+                    case 2:
+                        {
+                            var del = stack.Pop();
+
+                            if(del == maxes.Peek())
+                            {
+                                maxes.Pop();
+                            }
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine(maxes.Peek());
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
