b633a129e43d57e2d1bd01aa4b01877d0950a085
gva <gvanastasov@abv.bg>
2017-05-29 20:42:45 +0200

    first couple tasks

---
+++ 01_Stacks_Queues/stacks_queues/Reverse_Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Reverse_Strings
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+
+            var stk = new Stack<char>(str.Length);
+            foreach (var c in str)
+            {
+                stk.Push(c);
+            }
+
+            while (stk.Count != 0)
+            {
+                Console.Write(stk.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 01_Stacks_Queues/stacks_queues/SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SimpleCalculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+
+            var args = new Stack<string>(input.Reverse());
+
+            while (args.Count > 1)
+            {
+                var a = int.Parse(args.Pop());
+                var op = args.Pop();
+                var b = int.Parse(args.Pop());
+
+                if(op == "+")
+                {
+                    args.Push((a + b).ToString());
+                }
+                else if(op == "-")
+                {
+                    args.Push((a - b).ToString());
+                }
+            }
+
+            Console.WriteLine(args.Peek());
+        }
+    }
+}
