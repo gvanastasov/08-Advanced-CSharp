5a784350643688497f5f3050c76949a426549b23
gva <gvanastasov@abv.bg>
2017-05-29 20:50:32 +0200

    task 4 done

--- 01_Stacks_Queues/stacks_queues/DecimalToBinaryConverter/Program.cs
+++ 01_Stacks_Queues/stacks_queues/DecimalToBinaryConverter/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DecimalToBinaryConverter
+﻿namespace DecimalToBinaryConverter
 {
+    using System;
+    using System.Collections.Generic;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var num = int.Parse(Console.ReadLine());
+
+            if(num == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                var binary = new Stack<int>();
+                while (num > 0)
+                {
+                    binary.Push((num % 2 == 0) ? 0 : 1);
+                    num = num / 2;
+                }
+
+                while (binary.Count != 0)
+                {
+                    Console.Write(binary.Pop());
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
