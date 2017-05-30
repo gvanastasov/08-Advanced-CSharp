5a4b3f0b4b7d2832765824d9d278a295f634dd64
gva <gvanastasov@abv.bg>
2017-05-30 22:08:20 +0200

    task 7 done

---
+++ 01_Stacks_Queues/stacks_queues/BalancedParentheses/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        private static char[] openParentheses = new char[3] { '[', '{', '('};
+        private static char[] closeParentheses = new char[3] { ']', '}', ')' };
+
+        static void Main()
+        {
+            var str = Console.ReadLine();
+
+            var stack = new Stack<char>();
+            for (var i = 0; i < str.Length; i++)
+            {
+                var symbol = str[i];
+                if (openParentheses.Contains(symbol))
+                {
+                    stack.Push(symbol);
+                }
+                else if (closeParentheses.Contains(symbol))
+                {
+                    if (stack.Count > 0)
+                    {
+                        var last = stack.Pop();
+
+                        switch (last)
+                        {
+                            case '[':
+                                if(symbol != ']')
+                                {
+                                    Console.WriteLine("NO");
+                                    return;
+                                }
+                                break;
+                            case '{':
+                                if (symbol != '}')
+                                {
+                                    Console.WriteLine("NO");
+                                    return;
+                                }
+                                break;
+                            case '(':
+                                if (symbol != ')')
+                                {
+                                    Console.WriteLine("NO");
+                                    return;
+                                }
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+
+            if(stack.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
