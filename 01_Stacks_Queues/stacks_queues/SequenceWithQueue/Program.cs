65c8069ecf2b0f75f149332149f404281f68b83c
gva <gvanastasov@abv.bg>
2017-05-30 20:47:22 +0200

    ex 5 done

---
+++ 01_Stacks_Queues/stacks_queues/SequenceWithQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SequenceWithQueue
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var n = long.Parse(Console.ReadLine());
+
+            var queue = new Queue<long>();
+
+            queue.Enqueue(n);
+            for (int i = 1; i <= 50; i++)
+            {
+                var s = queue.Dequeue();
+                queue.Enqueue(s + 1);
+                queue.Enqueue(2 * s + 1);
+                queue.Enqueue(s + 2);
+
+                Console.Write(s + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
