2623e9a5555c7c32bfad8eb1c8b0e076c807de5b
gva <gvanastasov@abv.bg>
2017-05-30 22:18:22 +0200

    task 8 done

---
+++ 01_Stacks_Queues/stacks_queues/RecursiveFibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RecursiveFibonacci
+{
+    using System;
+    class Program
+    {
+        private static long[] memo;
+
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            memo = new long[n];
+
+            Console.WriteLine(recursiveFibonacci(n-1));
+        }
+
+        private static long recursiveFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            if (memo[n] != 0)
+            {
+                return memo[n];
+            }
+
+            memo[n] = recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+            return memo[n];
+        }
+    }
+}
