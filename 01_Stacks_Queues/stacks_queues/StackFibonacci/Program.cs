388abbfad9af87f0a9071adf50e3b5488681c95d
gva <gvanastasov@abv.bg>
2017-05-30 22:31:29 +0200

    task 9 done

---
+++ 01_Stacks_Queues/stacks_queues/StackFibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace StackFibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var stack = new Stack<long>();
+
+            stack.Push(0);
+            stack.Push(1);
+            for (int i = 0; i < n-1; i++)
+            {
+                var prev = stack.Pop();
+                var next = stack.Peek() + prev;
+
+                stack.Push(prev);
+                stack.Push(next);
+            }
+
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
