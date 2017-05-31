e732f1e74f3d9268f2c83d5e24c7ada4336a57fd
gva <gvanastasov@abv.bg>
2017-05-31 20:24:57 +0200

    finished ex1

---
+++ 02_Sets_Dictionaries/SetsDictionaries/UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,25 @@
+﻿namespace _01_UniqueUsernames
+{
+    using System;
+    using System.Collections.Generic;
+
+    class UniqueUsernames
+    {
+        static void Main()
+        {
+            var count = int.Parse(Console.ReadLine());
+
+            var usernames = new HashSet<string>();
+            for (int i = 0; i < count; i++)
+            {
+                var name = Console.ReadLine();
+                usernames.Add(name);
+            }
+
+            foreach (var name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
