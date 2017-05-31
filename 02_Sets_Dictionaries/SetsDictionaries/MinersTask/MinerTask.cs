67f8789479747eb81930fe2daec93a8f60eb7ed0
gva <gvanastasov@abv.bg>
2017-05-31 21:40:27 +0200

    task 6 finished

---
+++ 02_Sets_Dictionaries/SetsDictionaries/MinersTask/MinerTask.cs
@@ -0,0 +1,35 @@
+﻿namespace _06_MinersTask
+{
+    using System;
+    using System.Collections.Generic;
+
+    class MinerTask
+    {
+        static void Main()
+        {
+            var resources = new Dictionary<string, int>();
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+
+                if(cmd == "stop")
+                {
+                    break;
+                }
+
+                if(resources.ContainsKey(cmd) == false)
+                {
+                    resources.Add(cmd, 0);
+                }
+
+                var quantity = int.Parse(Console.ReadLine());
+                resources[cmd] += quantity;
+            }
+
+            foreach (var kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
