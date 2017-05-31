2fdee4c234f629b3450c1f9144cbf53423484fa1
gva <gvanastasov@abv.bg>
2017-05-31 19:43:45 +0200

    task 1 done

---
+++ 01_Stacks_Queues/stacks_queues/PoisonousPlants/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoisonousPlants
+{
+    class Program
+    {
+        static void Main()
+        {
+            var plantsCount = int.Parse(Console.ReadLine());
+            var plantsData = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            plantsData.Reverse();
+
+
+            var plants = new Queue<int?>();
+            foreach (var pData in plantsData)
+            {
+                plants.Enqueue(pData);
+            }
+
+            var day = 1;
+            while (true)
+            {
+                var before = plants.Where(p => p.HasValue).Count();
+
+                // kill plants
+                for (int i = 0; i < plants.Count; i++)
+                {
+                    var plant = plants.Dequeue();
+                    var left = plants.Peek();
+
+                    if(plant <= left)
+                    {
+                        plants.Enqueue(plant);
+                    }
+                    else
+                    {
+                        plants.Enqueue(null);
+                    }
+                }
+
+                var after = plants.Where(p => p.HasValue).Count();
+
+                Console.WriteLine("Before: " + before + " Survived: " + after);
+
+                if (before == after) // no plants died
+                {
+                    Console.WriteLine(day);
+                    break;
+                }
+                else
+                {
+                    day++;
+                }
+            }
+        }
+    }
+}
---
+++ 02_Sets_Dictionaries/SetsDictionaries/ParkingLot/ParkingLot.cs
@@ -0,0 +1,49 @@
+﻿
+namespace _01_ParkingLot
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    class ParkingLot
+    {
+        static void Main()
+        {
+            var parkingLot = new SortedSet<string>();
+
+            while (true)
+            {
+                var tokens = Regex.Split(Console.ReadLine(), ", ");
+
+                var cmd = tokens[0];
+                if(cmd == "END")
+                {
+                    break;
+                }
+
+                var plateNumber = tokens[1];
+                switch (cmd)
+                {
+                    case "IN":
+                        parkingLot.Add(plateNumber);
+                        break;
+                    case "OUT":
+                        parkingLot.Remove(plateNumber);
+                        break;
+                }
+            }
+
+            if(parkingLot.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in parkingLot)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
