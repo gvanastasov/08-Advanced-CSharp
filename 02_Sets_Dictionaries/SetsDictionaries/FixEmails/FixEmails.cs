8f85ce07b8a7efb7420d55d24b1f55a907220b67
gva <gvanastasov@abv.bg>
2017-05-31 21:49:07 +0200

    ex7 finished

---
+++ 02_Sets_Dictionaries/SetsDictionaries/FixEmails/FixEmails.cs
@@ -0,0 +1,36 @@
+﻿namespace _07_FixEmails
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    class FixEmails
+    {
+        static void Main()
+        {
+            var emails = new Dictionary<string, string>();
+            var emailRegex = new Regex("^.*@.*\\.(us|uk)$", RegexOptions.IgnoreCase);
+
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+
+                if(cmd == "stop")
+                {
+                    break;
+                }
+
+                var email = Console.ReadLine();
+
+                if(emailRegex.IsMatch(email) == false)
+                {
+                    emails.Add(cmd, email);
+                }
+            }
+
+            foreach (var kvp in emails)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
