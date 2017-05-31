9c9798d24737cc071791ba54c13048cc86d89b08
gva <gvanastasov@abv.bg>
2017-05-31 20:14:14 +0200

    finished task 3

---
+++ 02_Sets_Dictionaries/SetsDictionaries/CountSameValues/CountSameVals.cs
@@ -0,0 +1,32 @@
+﻿namespace _03_CountSameValues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class CountSameVals
+    {
+        static void Main()
+        {
+            var values = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse);
+
+            var occurences = new SortedDictionary<double, int>();
+            foreach (var v in values)
+            {
+                if(occurences.ContainsKey(v))
+                {
+                    occurences[v]++;
+                }
+                else
+                {
+                    occurences.Add(v, 1);
+                }
+            }
+
+            foreach (var kvp in occurences)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
