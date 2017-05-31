6b9702a67221c8886a7a54c9e778f8a3402406ba
gva <gvanastasov@abv.bg>
2017-05-31 21:19:59 +0200

    finished ex3

---
+++ 02_Sets_Dictionaries/SetsDictionaries/PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,30 @@
+﻿namespace _03_PeriodicTable
+{
+    using System;
+    using System.Collections.Generic;
+
+    class PeriodicTable
+    {
+        static void Main()
+        {
+            var count = int.Parse(Console.ReadLine());
+
+            var uniques = new SortedSet<string>();
+            for (int i = 0; i < count; i++)
+            {
+                var elements = Console.ReadLine().Split(' ');
+
+                foreach (var el in elements)
+                {
+                    uniques.Add(el);
+                }
+            }
+
+            foreach (var el in uniques)
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
