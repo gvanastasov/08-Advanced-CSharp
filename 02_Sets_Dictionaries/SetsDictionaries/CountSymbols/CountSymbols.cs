202099258476c5905a34547221a613812a4b946d
gva <gvanastasov@abv.bg>
2017-05-31 21:24:51 +0200

    ex4 finished

---
+++ 02_Sets_Dictionaries/SetsDictionaries/CountSymbols/CountSymbols.cs
@@ -0,0 +1,30 @@
+﻿namespace _04_CountSymbols
+{
+    using System;
+    using System.Collections.Generic;
+    class CountSymbols
+    {
+        static void Main()
+        {
+            var str = Console.ReadLine();
+
+            var occurances = new SortedDictionary<char, int>();
+            foreach (var c in str)
+            {
+                if(occurances.ContainsKey(c))
+                {
+                    occurances[c]++;
+                }
+                else
+                {
+                    occurances.Add(c, 1);
+                }
+            }
+
+            foreach (var kvp in occurances)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
