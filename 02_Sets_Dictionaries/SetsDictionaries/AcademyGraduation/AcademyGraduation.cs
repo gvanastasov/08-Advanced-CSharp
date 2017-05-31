672186be4aab6c6e66a1e9f5b86a8ece1e78b509
gva <gvanastasov@abv.bg>
2017-05-31 20:21:16 +0200

    finished task 4

---
+++ 02_Sets_Dictionaries/SetsDictionaries/AcademyGraduation/AcademyGraduation.cs
@@ -0,0 +1,29 @@
+﻿namespace _04_AcademyGraduation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var studentsCount = int.Parse(Console.ReadLine());
+
+            var students = new SortedDictionary<string, double[]>();
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                var name = Console.ReadLine();
+                var scores = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+                students.Add(name, scores);
+            }
+
+            foreach (var kvp in students)
+            {
+                Console.WriteLine($"{kvp.Key} is graduated with {kvp.Value.Average()}");
+            }
+        }
+    }
+}
