9ab841426f7ed5112fa3fdecdafe74fdbdd4340f
gva <gvanastasov@abv.bg>
2017-06-03 17:58:31 +0200

    task 1 done

---
+++ 03_Matrices/Matrices/SumMatrixElements/SumMatrixElements.cs
@@ -0,0 +1,34 @@
+﻿
+namespace _01_SumMatrixElements
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class SumMatrixElements
+    {
+        static void Main()
+        {
+            var size = Regex.Split(Console.ReadLine(), ", ");
+
+            var rows = int.Parse(size[0]);
+            var cols = int.Parse(size[1]);
+            var sum = 0;
+
+            var matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var rowData = Regex.Split(Console.ReadLine(), ", ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rowData[j];
+                    sum += rowData[j];
+                }
+            }
+
+            Console.WriteLine($"{rows}\n{cols}\n{sum}");
+        }
+    }
+}
