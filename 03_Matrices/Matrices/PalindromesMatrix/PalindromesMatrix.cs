3a89c907e6a3598a43ec3f3f766fa680943ec356
gva <gvanastasov@abv.bg>
2017-06-04 10:06:57 +0200

    ex 1 done

---
+++ 03_Matrices/Matrices/PalindromesMatrix/PalindromesMatrix.cs
@@ -0,0 +1,29 @@
+﻿namespace _01_PalindromesMatrix
+{
+    using System;
+
+    class PalindromesMatrix
+    {
+        static void Main()
+        {
+            var dimensions = Console.ReadLine().Split(' ');
+
+            var rows = int.Parse(dimensions[0]);
+            var cols = int.Parse(dimensions[1]);
+
+            var palindromes = new string[rows,cols];
+            for (int x = 'a', i = 0; i < rows; x++, i++)
+            {
+                for (int y = x, j = 0; j < cols; y++, j++)
+                {
+                    var f = (char)x;
+                    var m = (char)y;
+                    var p = string.Format($"{f}{m}{f}");
+                    palindromes[i, j] = p;
+                    Console.Write(p + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
