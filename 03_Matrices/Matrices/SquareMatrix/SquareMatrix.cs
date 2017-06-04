29cc7e7bffe2ea44067e6e9abacdf690cbfd9da8
gva <gvanastasov@abv.bg>
2017-06-04 13:23:03 +0200

    ex 3 done

---
+++ 03_Matrices/Matrices/SquareMatrix/SquareMatrix.cs
@@ -0,0 +1,58 @@
+﻿namespace _03_SquareMatrix
+{
+    using System;
+
+    class SquareMatrix
+    {
+        static void Main()
+        {
+            var size = Console.ReadLine().Split(' ');
+
+            var rows = int.Parse(size[0]);
+            var cols = int.Parse(size[1]);
+
+            var matrix = new char[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var chars = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = chars[j][0];
+                }
+            }
+
+            var sqMatrixCount = 0;
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    var isSqMatrix = true;
+                    var topLeftChar = matrix[i, j];
+
+                    for (int coord_x = i; coord_x < i + 2; coord_x++)
+                    {
+                        for (int coord_y = j; coord_y < j + 2; coord_y++)
+                        {
+                            if(matrix[coord_x, coord_y] != topLeftChar)
+                            {
+                                isSqMatrix = false;
+                                break;
+                            }
+                        }
+
+                        if(isSqMatrix == false)
+                        {
+                            break;
+                        }
+                    }
+
+                    sqMatrixCount += (isSqMatrix) ? 1 : 0;
+                }
+            }
+
+            Console.WriteLine(sqMatrixCount);
+        }
+    }
+}
