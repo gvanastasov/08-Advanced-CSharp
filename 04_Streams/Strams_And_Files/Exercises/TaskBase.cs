9934c1d244c3633321f2babd67439c45c50011f0
gva <gvanastasov@abv.bg>
2017-06-06 21:36:33 +0200

    added base task structure

---
+++ 04_Streams/Strams_And_Files/Exercises/TaskBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public abstract class TaskBase
+    {
+        private string _displayName = string.Empty;
+        public string DisplayName
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(_displayName))
+                {
+                    _displayName = Utils.SplitCamelCase(this.GetType().Name);
+                }
+                return _displayName;
+            }
+        }
+
+        public abstract void Execute();
+
+    }
+}
---
+++ 04_Streams/Strams_And_Files/Exercises/Utils.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercises
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public static class Utils
+    {
+        public static string SplitCamelCase(string source)
+        {
+            return string.Join(" ", Regex.Split(source, @"(?<!^)(?=[A-Z](?![A-Z]|$))"));
+        }
+    }
+}
