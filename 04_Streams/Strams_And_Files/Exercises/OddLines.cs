b7db77face598db5196d7365c04e6f326e1aa979
gva <gvanastasov@abv.bg>
2017-06-06 21:36:40 +0200

    finished task 1

---
+++ 04_Streams/Strams_And_Files/Exercises/OddLines.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class OddLines : TaskBase
+    {
+        private const string file = "../../../Resources/index.html";
+
+        public override void Execute()
+        {
+            using (StreamReader reader = new StreamReader(file))
+            {
+                int lineNumber = 0;
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if(lineNumber % 2 != 0)
+                    {
+                        Console.WriteLine($"Line[{lineNumber}]: {line}");
+                    }
+                    lineNumber++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
